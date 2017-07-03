1f77e486a3672b1350a7e9a77c3452e3ef7fda23
KleoPetroff <kleopetroff@gmail.com>
2017-07-04 01:28:17 +0300

    simplify random number generation

--- src/helpers/generateClass.js
+++ src/helpers/generateClass.js
@@ -1,9 +1,9 @@
-function generateRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function generateRandom(max) {
+  return Math.floor(Math.random() * max - 1);
 }
 
 function getRandomName(gradients) {
-  return gradients[generateRandom(1, gradients.length)];
+  return gradients[generateRandom(gradients.length)];
 }
 
 export default getRandomName;
